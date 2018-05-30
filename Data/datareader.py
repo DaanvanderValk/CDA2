ebf5e664d35be77c617ffe7d2570e4b903b62c58
Daan van der Valk <daanvdvalk@gmail.com>
2018-05-30 12:16:36 +0200

    Updated data reader: check for an attack at a particular time

--- Data/datareader.py
+++ Data/datareader.py
@@ -30,3 +30,28 @@ def read_4_series(feature):
 
 def read_test_series(feature):
     return pd.read_csv('../Data/BATADAL_test_dataset.csv', parse_dates=[0], date_parser=parser, index_col=0, squeeze=True, usecols=['DATETIME', feature])
+
+def attack_at_time(time):
+    attacks = [
+            # Dataset 4 attacks (1-7)
+            [pd.to_datetime('2016-09-13 23:00:00'), pd.to_datetime('2016-09-16 00:00:00')],
+            [pd.to_datetime('2016-09-26 11:00:00'), pd.to_datetime('2016-09-27 10:00:00')],
+            [pd.to_datetime('2016-10-09 09:00:00'), pd.to_datetime('2016-10-11 20:00:00')],
+            [pd.to_datetime('2016-10-29 19:00:00'), pd.to_datetime('2016-11-02 16:00:00')],
+            [pd.to_datetime('2016-11-26 17:00:00'), pd.to_datetime('2016-11-29 04:00:00')],
+            [pd.to_datetime('2016-12-06 07:00:00'), pd.to_datetime('2016-12-10 04:00:00')],
+            [pd.to_datetime('2016-12-14 15:00:00'), pd.to_datetime('2016-12-19 04:00:00')],
+            # Test set attacks (8-14)
+            [pd.to_datetime('2017-01-16 09:00:00'), pd.to_datetime('2017-01-19 06:00:00')],
+            [pd.to_datetime('2017-01-30 08:00:00'), pd.to_datetime('2017-02-02 00:00:00')],
+            [pd.to_datetime('2017-02-09 03:00:00'), pd.to_datetime('2017-02-10 09:00:00')],
+            [pd.to_datetime('2017-02-12 01:00:00'), pd.to_datetime('2017-02-13 07:00:00')],
+            [pd.to_datetime('2017-02-24 05:00:00'), pd.to_datetime('2017-02-28 08:00:00')],
+            [pd.to_datetime('2017-03-10 14:00:00'), pd.to_datetime('2017-03-13 21:00:00')],
+            [pd.to_datetime('2017-03-25 20:00:00'), pd.to_datetime('2017-03-27 01:00:00')]
+    ]
+    for attack in attacks:
+        if attack[0] <= time <= attack[1]:
+            return True
+    
+    return False
