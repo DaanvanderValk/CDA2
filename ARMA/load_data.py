689f13f5c03f5167af941eb036dedecdf52034a8
Daan van der Valk <daanvdvalk@gmail.com>
2018-05-30 09:51:38 +0200

    Simplified imports

--- ARMA/load_data.py
+++ ARMA/load_data.py
@@ -4,15 +4,11 @@ Created on Wed May 30 09:34:59 2018
 
 @author: Daan
 """
-
+# Import functions from data reader:
+# read_3_df(), read_4_df(), read_test_df(), read_3_series(feature), read_4_series(feature), read_test_series(feature)
 import sys
 sys.path.append('../')
-from Data.datareader import read_3_df
-from Data.datareader import read_4_df
-from Data.datareader import read_test_df
-from Data.datareader import read_3_series
-from Data.datareader import read_4_series
-from Data.datareader import read_test_series
+from Data.datareader import *
 
-df = read_3_df()
+df = read_3_series('L_T1')
 print(df)
